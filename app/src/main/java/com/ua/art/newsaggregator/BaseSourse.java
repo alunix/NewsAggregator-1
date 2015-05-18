473639a4a0670cd8dc1ce1f41e722547b5b7f021
artrak <artrak9@gmail.com>
2015-05-19 00:02:03 +0300

    Initial commit

---
+++ app/src/main/java/com/ua/art/newsaggregator/BaseSourse.java
@@ -0,0 +1,14 @@
+package com.ua.art.newsaggregator;
+
+import java.util.ArrayList;
+
+public class BaseSourse {
+    private static ArrayList<String> selectRecource = new ArrayList<>();
+
+    public BaseSourse() {
+        selectRecource.add("ukr.net");
+        selectRecource.add("liga.net");
+        selectRecource.add("ukr.net");
+        selectRecource.add("korrespondent.net");
+    }
+}
---
+++ app/src/main/java/com/ua/art/newsaggregator/SelectResource.java
@@ -0,0 +1,7 @@
+package com.ua.art.newsaggregator;
+
+/**
+ * Created by Артем on 18.05.2015.
+ */
+public class SelectResource {
+}
