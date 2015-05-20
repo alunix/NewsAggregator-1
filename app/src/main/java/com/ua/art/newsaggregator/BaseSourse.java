52735e840f7d7eea515b253a7756bc49e5f2d13c
artrak <artrak9@gmail.com>
2015-05-20 22:01:07 +0300

    20.05.2015

--- app/src/main/java/com/ua/art/newsaggregator/BaseSourse.java
+++ app/src/main/java/com/ua/art/newsaggregator/BaseSourse.java
@@ -3,12 +3,42 @@ package com.ua.art.newsaggregator;
 import java.util.ArrayList;
 
 public class BaseSourse {
-    private static ArrayList<String> selectRecource = new ArrayList<>();
+
+    private static ArrayList<String> idSelectSourceArr = new ArrayList<>();         // id Source
+    private static ArrayList<String> nameSelectSourceArr = new ArrayList<>();       // name Source
+    private static ArrayList<String> imgSelectSourceArr = new ArrayList<>();        // img-logo Source
 
     public BaseSourse() {
-        selectRecource.add("ukr.net");
-        selectRecource.add("liga.net");
-        selectRecource.add("ukr.net");
-        selectRecource.add("korrespondent.net");
+        idSelectSourceArr.add("ukr_net");
+        nameSelectSourceArr.add("ukr.net");
+        imgSelectSourceArr.add("@drawable/abc_btn_switch_to_on_mtrl_00012");
+        idSelectSourceArr.add("112");
+        nameSelectSourceArr.add("112");
+        imgSelectSourceArr.add("@drawable/abc_btn_switch_to_on_mtrl_00012");
+        idSelectSourceArr.add("pdynPluaOdyn");
+        nameSelectSourceArr.add("1+1");
+        imgSelectSourceArr.add("@drawable/abc_btn_switch_to_on_mtrl_00012");
+        idSelectSourceArr.add("ctb");
+        nameSelectSourceArr.add("ctb");
+        imgSelectSourceArr.add("@drawable/abc_btn_switch_to_on_mtrl_00012");
+    }
+
+    public static ArrayList<String> getIdSelectSourceArr() {
+        return idSelectSourceArr;
+    }
+    public static void setIdSelectSourceArr(ArrayList<String> idSelectSourceArr) {
+        BaseSourse.idSelectSourceArr = idSelectSourceArr;
+    }
+    public static ArrayList<String> getNameSelectSourceArr() {
+        return nameSelectSourceArr;
+    }
+    public static void setNameSelectSourceArr(ArrayList<String> nameSelectSourceArr) {
+        BaseSourse.nameSelectSourceArr = nameSelectSourceArr;
+    }
+    public static ArrayList<String> getImgSelectSourceArr() {
+        return imgSelectSourceArr;
+    }
+    public static void setImgSelectSourceArr(ArrayList<String> imgSelectSourceArr) {
+        BaseSourse.imgSelectSourceArr = imgSelectSourceArr;
     }
 }
--- app/src/main/java/com/ua/art/newsaggregator/SelectResource.java
+++ app/src/main/java/com/ua/art/newsaggregator/SelectResource.java
@@ -1,7 +1,56 @@
 package com.ua.art.newsaggregator;
 
-/**
- * Created by Артем on 18.05.2015.
- */
-public class SelectResource {
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
+import java.util.ArrayList;
+
+public class SelectResource extends Activity {
+    private TableLayout tableLayout_SelectSours;
+    private ArrayList<TableRow> tableRowsArr_SelectSours;
+    private ArrayList<Button> buttonViewsArr_SelectSours;
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        tableLayout_SelectSours = (TableLayout)findViewById(R.id.TableLayout_SelectSours);
+        tableRowsArr_SelectSours = new ArrayList<>();
+
+        // To add TableRow in TableLayout
+        float tempIdSize = BaseSourse.getIdSelectSourceArr().size()/3;
+        if ((int)tempIdSize < tempIdSize)       // Quantity TableRow / количество TableRow
+            tempIdSize = (int)tempIdSize + 1;
+        else
+            tempIdSize = (int)tempIdSize;
+
+        for (int i = 0; i < tempIdSize; i++) {
+            tableRowsArr_SelectSours.add(new TableRow(this));
+            tableLayout_SelectSours.addView(tableRowsArr_SelectSours.get(i));
+        }
+
+
+        for (int i = 0; i < BaseSourse.getIdSelectSourceArr().size(); i++){           // Array add Button
+            buttonViewsArr_SelectSours.add(new Button(this));
+            buttonViewsArr_SelectSours.get(i).setId(i);      /// ?????????????????????????????????????????????????????????????????????????
+            buttonViewsArr_SelectSours.get(i).setText(BaseSourse.getNameSelectSourceArr().get(i));
+            buttonViewsArr_SelectSours.get(i).setBackground(Drawable.createFromPath("@mipmap/".concat(BaseSourse.getImgSelectSourceArr().get(i))));
+        }
+
+        int idSizeSourceArr = 0;
+        for (TableRow elem : tableRowsArr_SelectSours){                             // add imageView
+            for (int i = 0; i < 3; i++, idSizeSourceArr++) {
+                if (BaseSourse.getIdSelectSourceArr().size() > idSizeSourceArr){
+                    elem.addView(buttonViewsArr_SelectSours.get(idSizeSourceArr));
+                }
+            }
+        }
+    }
 }
