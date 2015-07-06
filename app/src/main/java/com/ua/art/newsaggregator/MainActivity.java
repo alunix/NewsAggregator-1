9a90ac1a5efee68276bd136357ab34c5f8c8eab8
artrak <artrak9@gmail.com>
2015-07-06 16:59:42 +0300

    06.07.2015

--- app/src/main/java/com/ua/art/newsaggregator/MainActivity.java
+++ app/src/main/java/com/ua/art/newsaggregator/MainActivity.java
@@ -1,169 +1,58 @@
 package com.ua.art.newsaggregator;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
 
-import com.example.android.common.activities.SampleActivityBase;
-import com.example.android.slidingtabscolors.SlidingTabsColorsFragment;
+import com.astuetz.PagerSlidingTabStrip;
+import com.ua.art.newsaggregator.adapter.MainAdapter;
+import com.ua.art.newsaggregator.view.LoginActivity;
 
 
-public class MainActivity extends SampleActivityBase {
+public class MainActivity extends ActionBarActivity {
+
+    private static final int LOGIN_ACTIVITY = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        new Preferences(this);
 
-        if (savedInstanceState == null) {
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            SlidingTabsColorsFragment fragment = new SlidingTabsColorsFragment();
-            transaction.replace(R.id.sample_content_fragment, fragment);
-            transaction.commit();
+        if (isLogin()) {
+            startApp();
+        } else {
+            loginUser();
         }
-
-
-//        LinearLayout tableLayout = (LinearLayout) findViewById(R.id.linearSelMod);
-//        NewsTable newsTable = new NewsTable(this, tableLayout);
-//        newsTable.createTable();
-
-
-//        Intent intent = new Intent(this, NewsTable.class);
-//        startActivity(intent);
-
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        int id = item.getItemId();
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
+    private boolean isLogin() {
+        return Preferences.getLogin().equals("");
+    }
+
+    private void loginUser() {
+        startActivityForResult(new Intent(this, LoginActivity.class), LOGIN_ACTIVITY);
+    }
+
+    private void startApp() {
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(new MainAdapter(getSupportFragmentManager()));
+        PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        tabs.setViewPager(pager);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case LOGIN_ACTIVITY:{
+                if(resultCode == RESULT_OK){
+                    startApp();
+                } else {
+                    //ERROR dialog, redirect to login
+                }
+            }
+        }
+    }
 }
-
-
-//public class MainActivity extends ActionBarActivity {
-//
-////    private NavigationDrawerFragment mNavigationDrawerFragment;
-////
-////    private CharSequence mTitle;
-//
-//    public static String LOG_TAG = "my_log";
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        ActionBar actionBar = getSupportActionBar();
-//        actionBar.hide();
-//        setContentView(R.layout.main);
-//
-//
-//
-//
-//
-//
-////        mNavigationDrawerFragment = (NavigationDrawerFragment)
-////                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-////        mTitle = getTitle();
-////
-////        // Set up the drawer.
-////        mNavigationDrawerFragment.setUp(
-////                R.id.navigation_drawer,
-////                (DrawerLayout) findViewById(R.id.drawer_layout));
-//
-////        Intent intent = new Intent(this, ButtonAddTable.class);
-////        startActivity(intent);
-//    }
-
-//    @Override
-//    public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
-//        FragmentManager fragmentManager = getSupportFragmentManager();
-//        fragmentManager.beginTransaction()
-//                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-//                .commit();
-//    }
-
-//    public void onSectionAttached(int number) {
-//        switch (number) {
-//            case 1:
-//                mTitle = getString(R.string.title_section1);
-//                break;
-//            case 2:
-//                mTitle = getString(R.string.title_section2);
-//                break;
-//            case 3:
-//                mTitle = getString(R.string.title_section3);
-//                break;
-//        }
-//    }
-
-//    public void restoreActionBar() {
-//        ActionBar actionBar = getSupportActionBar();
-//        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-//        actionBar.setDisplayShowTitleEnabled(true);
-//        actionBar.setTitle(mTitle);
-//    }
-//
-//
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-//            getMenuInflater().inflate(R.menu.main, menu);
-//            restoreActionBar();
-//            return true;
-//        }
-//        return super.onCreateOptionsMenu(menu);
-//    }
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
-
-//    public static class PlaceholderFragment extends Fragment {
-//
-//        private static final String ARG_SECTION_NUMBER = "section_number";
-//
-//        public static PlaceholderFragment newInstance(int sectionNumber) {
-//            PlaceholderFragment fragment = new PlaceholderFragment();
-//            Bundle args = new Bundle();
-//            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-//            fragment.setArguments(args);
-//            return fragment;
-//        }
-//
-//        public PlaceholderFragment() {
-//        }
-//
-//        @Override
-//        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                                 Bundle savedInstanceState) {
-//            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-//            return rootView;
-//        }
-//
-//        @Override
-//        public void onAttach(Activity activity) {
-//            super.onAttach(activity);
-//            ((MainActivity) activity).onSectionAttached(
-//                    getArguments().getInt(ARG_SECTION_NUMBER));
-//        }
-//    }
-
-
