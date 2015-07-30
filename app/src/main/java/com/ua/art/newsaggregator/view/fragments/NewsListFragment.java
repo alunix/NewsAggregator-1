de4624e3875ce9333ffa09a27ef37d1fe65cd11f
artrak <artrak9@gmail.com>
2015-07-30 13:35:12 +0300

    30.07.2015 + Updated HttpClient (openConnection()) + NewsListFragment

--- app/src/main/java/com/ua/art/newsaggregator/view/fragments/NewsListFragment.java
+++ app/src/main/java/com/ua/art/newsaggregator/view/fragments/NewsListFragment.java
@@ -28,15 +28,29 @@ public class NewsListFragment extends Fragment {
     protected static final String URL = "http://news.webstudia.dp.ua/index/test";  // http://api.androidhive.info/contacts/
 
 
+//    private static final String TAG_NEWS = "news";
+//    private static final String TAG_CATEGORY = "category";
+//    private static final String TAG_SOURCE = "source";
+//    private static final String TAG_ID_ITEM = "id_item";
+//    private static final String TAG_TITLE = "title";
+//    private static final String TAG_LINK = "link";
+//    private static final String TAG_DESCRIPTION = "description";
+//    private static final String TAG_IMG = "img";
+
     private static final String TAG_NEWS = "news";
-    private static final String TAG_CATEGORY = "category";
-    private static final String TAG_SOURCE = "source";
-    private static final String TAG_ID_ITEM = "id_item";
-    private static final String TAG_TITLE = "title";
+    private static final String TAG_ID = "id";
+    private static final String TAG_NAME = "name";
     private static final String TAG_LINK = "link";
     private static final String TAG_DESCRIPTION = "description";
-    private static final String TAG_IMG = "img";
-    private static final String TAG_DATE = "date";
+    private static final String TAG_IMAGE = "image";
+    private static final String TAG_PUBDATE = "pubDate";
+    private static final String TAG_TEXT = "text";
+    private static final String TAG_MODULEID = "moduleId";
+    private static final String TAG_CATEGORYID = "categoryId";
+    private static final String TAG_SOURCEID = "sourceId";
+
+
+    //private static final String TAG_DATE = "date";
 
     private ListView lv;
     private JSONArray news = null;
@@ -70,45 +84,57 @@ public class NewsListFragment extends Fragment {
         @Override
         protected Void doInBackground(Void... arg0) {
             HttpClient sh = new HttpClient();
-            String param = "{\"dsdasds\" : \"dsadsdas\"}";
+            String param = "{" +
+                    "\"moduleId\" : \"1\"," +
+                    "\"categoryId\" : \"1\"," +
+                    "\"sourceId\" : \"1\"," +
+                    "\"pubDate\" : \"thu, 30 jul 2015 07:26:46 +0300\"," +
+                    "\"quantity\" : \"1\"," +
+                    "\"older\" : \"true\"" +
+                    "}";
             String jsonStr = sh.sendPost(URL, param);
 
-            byte[] utf8 = new byte[0];
-            try {
-                utf8 = jsonStr.getBytes("UTF-8");
-                jsonStr = new String(utf8, "UTF-8");
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
+//            byte[] utf8 = new byte[0];
+//            try {
+//                utf8 = jsonStr.getBytes("UTF-8");
+//                jsonStr = new String(utf8, "UTF-8");
+//            } catch (UnsupportedEncodingException e) {
+//                e.printStackTrace();
+//            }
 
 
 
             Log.d("Response: ", "> " + jsonStr);
             if (jsonStr != null) {
                 try {
-
                     JSONObject jsonObj = new JSONObject(jsonStr);
+                    //JSONObject jsonObj = new JSONObject("{\"phonetype\":\"N95\",\"cat\":\"WP\"}");
                     news = jsonObj.getJSONArray(TAG_NEWS);
                     for (int i = 0; i < news.length(); i++) {
                         JSONObject c = news.getJSONObject(i);
 
-                        String category = c.getString(TAG_CATEGORY);
-                        String source = c.getString(TAG_SOURCE);
-                        String id_item = c.getString(TAG_ID_ITEM);
-                        String title = c.getString(TAG_TITLE);
+                        String id = c.getString(TAG_ID);
+                        String name = c.getString(TAG_NAME);
                         String link = c.getString(TAG_LINK);
                         String description = c.getString(TAG_DESCRIPTION);
-                        String img = c.getString(TAG_IMG);
-                        String date = c.getString(TAG_DATE);
+                        String image = c.getString(TAG_IMAGE);
+                        String pubDate = c.getString(TAG_PUBDATE);
+                        String text = c.getString(TAG_TEXT);
+                        String moduleId = c.getString(TAG_MODULEID);
+                        String categoryId = c.getString(TAG_CATEGORYID);
+                        String sourceId = c.getString(TAG_SOURCEID);
 
                         HashMap<String, String> contact = new HashMap<>();
-                        contact.put(TAG_CATEGORY, category);
-                        contact.put(TAG_SOURCE, source);
-                        contact.put(TAG_TITLE, title);
-                        contact.put(TAG_ID_ITEM, id_item);
+                        contact.put(TAG_ID, id);
+                        contact.put(TAG_NAME, name);
+                        contact.put(TAG_LINK, link);
                         contact.put(TAG_DESCRIPTION, description);
-                        contact.put(TAG_IMG, img);
-                        contact.put(TAG_DATE, date);
+                        contact.put(TAG_IMAGE, image);
+                        contact.put(TAG_PUBDATE, pubDate);
+                        contact.put(TAG_TEXT, text);
+                        contact.put(TAG_MODULEID, moduleId);
+                        contact.put(TAG_CATEGORYID, categoryId);
+                        contact.put(TAG_SOURCEID, sourceId);
                         newsList.add(contact);
                     }
                 } catch (JSONException e) {
@@ -129,8 +155,8 @@ public class NewsListFragment extends Fragment {
 //            }
             ListAdapter adapter = new SimpleAdapter(
                     getActivity(), newsList,
-                    R.layout.item_list_news, new String[]{TAG_TITLE, TAG_DESCRIPTION,
-                    TAG_SOURCE, TAG_DATE}, new int[]{R.id.titleItemText,
+                    R.layout.item_list_news, new String[]{TAG_NAME, TAG_DESCRIPTION,
+                    TAG_SOURCEID, TAG_PUBDATE}, new int[]{R.id.titleItemText,
                     R.id.deskripItemText, R.id.sourseItemText, R.id.dateItemText});
 
             lv.setAdapter(adapter);
