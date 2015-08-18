78fb0f513ba0a29f5531273cafc65f9673cef1b1
Артем <saint@ukr.net>
2015-08-18 06:56:53 +0300

    18.08.2015 HOME v2 list, надо Таймер для обновленеия новых новостей, сохранение выбора категорий в настройки, а от туда подсчет процента и запрос на Сервер.

--- app/src/main/java/com/ua/art/newsaggregator/controller/db/CountSelectCategory.java
+++ app/src/main/java/com/ua/art/newsaggregator/controller/db/CountSelectCategory.java
@@ -1,4 +1,35 @@
-package com.ua.art.newsaggregator.controller.db;
+package com.ua.art.newsaggregator.controller;
+
+import com.ua.art.newsaggregator.model.News;
+import com.ua.art.newsaggregator.service.Settings;
 
 public class CountSelectCategory {
+
+    public CountSelectCategory() {
+        addSelectCategory();
+        countSumSelectCategory();
+    }
+
+    public void addSelectCategory(){    // save selectCategory to Settings.selectCategory
+        Settings.nameSelectCategory.clear();
+        for (News key : News.values()){
+            if(String.valueOf(key.isClicked()) == "true"){  // if select Category - true
+//                Settings.selectCategory.put(
+//                        String.valueOf(key.getName()), String.valueOf(key.isClicked())
+//                );
+//                Settings.nameSelectCategory.put(
+//                        String.valueOf(key.getName()), String.valueOf(key.getName())
+//                );
+                //Settings.selectCategory.add(String.valueOf(key.getName()));
+                Settings.nameSelectCategory.add(String.valueOf(key.getName()));
+            }
+        }
+    }
+
+    public void countSumSelectCategory(){
+        if (Settings.nameSelectCategory.size() != 0)
+            Settings.sumItemOneCategory = Settings.maxNewsList/Settings.nameSelectCategory.size();
+        else
+            Settings.sumItemOneCategory = Settings.maxNewsList;
+    }
 }
